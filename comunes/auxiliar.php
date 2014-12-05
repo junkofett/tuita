7eacc339c9bf0e3cd4d53265154b9d6557d5741d
Leonardo Montilla <leonardomo84@gmail.com>
2014-12-05 22:22:51 +0100

    Muestra tuits en index

--- comunes/auxiliar.php
+++ comunes/auxiliar.php
@@ -3,3 +3,12 @@
     return pg_connect("host=localhost user=tuita password=tuita
                        dbname=tuita");
   }
+
+  function redireccion_login(){
+      header("Location: login/login.php");
+  }
+
+  function limpiar_datos_post(){
+      foreach ($_POST as $key => $value)
+        $_POST["$key"] = trim($value);
+    }
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,18 +10,59 @@
 
     $con = conectar();
 
-    function mostrar_tuits(){
+    $nick = "";
+    $usuario = devolver_usuario();
+    $tuits = devuelve_tuits();
+
+    function devolver_usuario(){
       global $con;
+      global $nick;
 
-      $res = pg_query_params($con, "select *
-                                      from usuarios
-                                    where id = $1", [$_SESSION['usuario']]);
-      $fila = pg_fetch_assoc($res);
+      if(!isset($_SESSION['usuario'])):
+        redireccion_login();
+      else:
+        $res = pg_query_params($con, "select *
+                                        from usuarios
+                                       where id::text = $1", 
+                                            [$_SESSION['usuario']]);
 
-      echo $fila['nick'];
+        if(pg_num_rows($res) != 1)
+          redireccion_login();
+        else{
+          $nick = pg_fetch_assoc($res)['nick'];
+          return $_SESSION['usuario'];
+        }
+
+      endif;
     }
 
-    mostrar_tuits();
+    function devuelve_tuits(){
+      global $con;
+      global $usuario;
+      
+      $res = pg_query_params($con, "select * 
+                                      from tuits
+                                      where usuarios_id = $1", [$usuario]);
+
+
+      return pg_fetch_all($res);
+    }
   ?>
+
+  <p>Usuario: <?= $nick ?></p>  <?php
+
+  foreach ($tuits as $tuit): ?>
+
+    <article>
+      <p>Mensaje:</p>
+      <section>
+        <?= $tuit['mensaje'] ?>
+      </section>
+      <section>
+        Fecha: <?= $tuit['fecha'] ?>
+      </section>
+    </article> <?php
+
+  endforeach; ?>
   </body>
 </html>
